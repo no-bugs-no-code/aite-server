a1de9ecc9801463dd1f6a289e0241e2a2df13b45
priverg163 <priverg163@dgsw.hs.kr>
2024-07-17 09:34:59 +0900

    Initial commit

---
+++ src/main/java/com/example/aiet_server/AietServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiet_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AietServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AietServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aiet_server/AietServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiet_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AietServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
